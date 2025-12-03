b1bf7a0a96e6b7d71c00013bf050a02c4f37559b
doxajay <99846671+doxajay@users.noreply.github.com>
2025-12-03 11:56:03 -0700

    Add resource group and policy assignment for public IPs
    Added a resource group and policy assignment for denying public IPs.

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # recent provider that supports policy_assignment
     }
   }
 }
@@ -11,11 +13,22 @@ provider "azurerm" {
   features {}
 }
 
-# Enable Azure Policy for Cost Optimization
-resource "azurerm_policy_assignment" "deny_public_ip" {
-  name                 = "policy-deny-public-ip"
-  scope                = data.azurerm_subscription.primary.id
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/2d9cae02-6151-4a68-83c7-3a7db3a0e0a9"
+# Resource group just to have a scope
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-policy-demo"
+  location = "canadacentral"
 }
 
-data "azurerm_subscription" "primary" {}
+# Example: built-in policy definition - deny public IPs
+data "azurerm_policy_definition" "deny_public_ip" {
+  display_name = "Not allowed resource types"
+  # or use a specific built-in name / id; this is just an example
+}
+
+resource "azurerm_policy_assignment" "deny_public_ip_assignment" {
+  name                 = "deny-public-ip-assignment"
+  scope                = azurerm_resource_group.rg.id
+  policy_definition_id = data.azurerm_policy_definition.deny_public_ip.id
+
+  description = "Assignment to deny public exposures in this RG"
+}
